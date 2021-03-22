e0379e611a631c60ee0258cbe50c5afe9710409e
Мария Светлая <merry76light@gmail.com>
2021-03-22 14:46:50 +0300

    feat: add fullscreen

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -13,30 +13,26 @@ const startSound = (event)=>{
       pianoKey.forEach((el) => {
         if(el.classList.contains('piano-key-active')) {
           el.classList.remove('piano-key-active');
-          el.classList.remove('piano-key-active-pseudo');
+        el.classList.remove('piano-key-active-pseudo');
         }
       });
-  
-      if(event.target.classList.contains('piano-key')){
         const note = event.target.dataset.note;
         const url = `assets/audio/${note}.mp3`;
         playAudio(url);
-      }  
       event.target.classList.add('piano-key-active');
       event.target.classList.add('piano-key-active-pseudo');
     }
 }
-
 const stopSound = (event) => {
   if(event.target.classList.contains('piano-key')) {
     event.target.classList.remove('piano-key-active');
     event.target.classList.remove('piano-key-active-pseudo');
   }
 }
+
 // проводим по клавишам нажатой мышкой
 const overMouse = (event)=>{
   event.target.classList.add("piano-key-active");
-  event.target.classList.add('piano-key-active-pseudo');
   startSound(event);
    pianoKey.forEach((elem) =>{
     elem.addEventListener("mouseover", startSound);
@@ -46,8 +42,7 @@ const overMouse = (event)=>{
 
 const outMouse = (event) => {
   event.target.classList.remove("piano-key-active");
-  event.target.classList.remove('piano-key-active-pseudo');
-  stopSound(event);
+  //stopSound(event);
   pianoKey.forEach((elem) =>{
     elem.removeEventListener("mouseover", startSound);
     elem.removeEventListener("mouseout", stopSound);
@@ -59,6 +54,7 @@ piano.addEventListener('mouseup', outMouse);
 
 // звук при нажатии на клавиатуру
 window.addEventListener('keydown', (event) => {
+ if (event.repeat === true) return;
   const letter = window.event.code;
   pianoKey.forEach((el)=> {
       if (letter ===  el.dataset.code){
@@ -77,10 +73,12 @@ window.addEventListener('keyup', (event) => {
       el.classList.remove('piano-key-active');
   });
 });
+
 //меняем кнопки notes/letter
 const buttonConteiner = document.querySelector(".btn-container");
 const button = document.querySelectorAll(".btn");
 const buttonLetters = document.querySelector(".btn-letters");
+console.log(buttonConteiner);
 
 const changeLetters = (event) =>{
   if (event.target.classList.contains("btn")){
@@ -94,7 +92,7 @@ const changeLetters = (event) =>{
 
   if (buttonLetters.classList.contains('btn-active')){
     pianoKey.forEach((el)=> {
-      el.classList.add('piano-key-letter')
+      el.classList.add('piano-key-letter');
     });
   }
   else {
@@ -104,3 +102,14 @@ const changeLetters = (event) =>{
   }
 };
 buttonConteiner.addEventListener('click', changeLetters)
+
+//полный экран
+const buttonFullScreen = document.querySelector(".fullscreen");
+
+const changeScreen = () => {
+  if(document.fullscreenElement === null){
+    document.documentElement.requestFullscreen();
+  }
+  else document.exitFullscreen();
+}
+buttonFullScreen.addEventListener('click', changeScreen);
