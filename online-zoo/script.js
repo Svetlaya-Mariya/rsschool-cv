296079be6064f556c55f0ca24ffb88a37beb3250
Мария Светлая <merry76light@gmail.com>
2021-04-01 12:48:59 +0300

    feat: add lending

---
+++ online-zoo/script.js
@@ -0,0 +1,11 @@
+/*const circle = document.querySelector('.circl');
+console.log(circle);
+
+circle.addEventListener("mouseover", handler);
+circle.addEventListener("mouseout", handler1);
+function handler1(){
+    circle.setAttribute('src', 'assets/image/landing/pets/ellips.png');
+}
+function handler() {
+    circle.setAttribute('src', 'assets/image/landing/pets/green.png');
+}*/
